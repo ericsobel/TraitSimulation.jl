3c8dd089a90e83363229d18408d4d8010c4a0991
huwenboshi <shihuwenbo@ucla.edu>
2016-08-21 18:33:15 -0700

    updated doc

--- test/test.jl
+++ test/test.jl
@@ -5,9 +5,10 @@ include("../src/TraitSimulation.jl")
 using DataFrames, TraitSimulation
 
 # create data frame for testing
-df = convert(DataFrame, rand(10,3))
-names!(df, [:U, :V, :W])
+df = convert(DataFrame, rand(10,6))
+names!(df, [:A, :B, :C, :D, :E, :F])
 
 # simulate from the model
-sim_model = Model(T ~ U*2V+2log(W), IdentityLink(), NormalResponse(1.0))
+formula = T ~ A+2B*C+3log(D*(E+F))
+sim_model = Model(formula, IdentityLink(), NormalResponse(1.0))
 simulate(sim_model, df)
