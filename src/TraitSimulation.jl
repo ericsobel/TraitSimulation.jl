22031b633adf4eb577f3fa4009c82e96bbebfdbe
huwenboshi <shihuwenbo@ucla.edu>
2016-08-26 09:04:01 -0700

    moved macro to inside of module

--- src/TraitSimulation.jl
+++ src/TraitSimulation.jl
@@ -6,6 +6,7 @@ module TraitSimulation
 
 export Model,
        simulate,
+       @vc, ⊗,
        NormalResponse,
        PoissonResponse,
        ExponentialResponse,
@@ -294,22 +295,28 @@ function simulate(model::Model, data_frame::DataFrame;
 
 end
 
-end # end module
+"""
+Define kronecker product operator
+"""
+⊗(A, B) = kron(A,B)
 
 """
 Parse a variance component
 """
 macro vc(expr::Expr)
+  
   expr_str = string(expr)
-  quote
+  num_vc = size(expr.args,1)
+  
+  ret_expr = quote
     expr = parse($expr_str)
     [VarianceComponent(eval(expr.args[i].args[2]),
                        eval(expr.args[i].args[3]))
-     for i=2:size(expr.args,1)]
+     for i=2:$num_vc]
   end
+
+  return esc(ret_expr)
+
 end
 
-"""
-Define kronecker product operator
-"""
-⊗(A, B) = kron(A,B)
+end # end module
