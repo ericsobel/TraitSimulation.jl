04fc3f75ef1bb02d08684d5e3bd6cfc2ed6626e8
huwenboshi <shihuwenbo@ucla.edu>
2016-08-28 14:44:17 -0700

    moved the vc macro to model_specifications.jl

--- src/TraitSimulation.jl
+++ src/TraitSimulation.jl
@@ -29,32 +29,9 @@ export Model,
 using DataFrames,
       Distributions
 
-
-
-"""
-Parse an expression specifying the covariance matrix of the random effects
-Returns code for constructing the array of VarianceComponent
-"""
-macro vc(expr::Expr)
-  
-  # TODO: add code to check validity of expr before parseing it
-  # throw an exception if the expr doesn't follow the syntax
-
-  expr_str = string(expr)
-  
-  ret_expr = quote
-    [VarianceComponent(eval(parse($expr_str).args[i].args[2]),
-                       eval(parse($expr_str).args[i].args[3]))
-     for i=2:size(parse($expr_str).args,1)]
-  end
-
-  return esc(ret_expr)
-
-end
-
 include("link_functions.jl")
 include("response_distributions.jl")
 include("model_specifications.jl")
 include("generate_simulations.jl")
 
-end # end module
+end
--- src/model_specifications.jl
+++ src/model_specifications.jl
@@ -57,3 +57,24 @@ Model(formula::Union{Formula, Vector{Formula}},
   link::Union{LinkFunction, Vector{LinkFunction}},
   resp_dist::Union{ResponseDistribution, Vector{ResponseDistribution}}) =
 Model(formula, Vector{VarianceComponent}(), link, resp_dist)
+
+"""
+Parse an expression specifying the covariance matrix of the random effects
+Returns code for constructing the array of VarianceComponent
+"""
+macro vc(expr::Expr)
+  
+  # TODO: add code to check validity of expr before parseing it
+  # throw an exception if the expr doesn't follow the syntax
+
+  expr_str = string(expr)
+  
+  ret_expr = quote
+    [VarianceComponent(eval(parse($expr_str).args[i].args[2]),
+                       eval(parse($expr_str).args[i].args[3]))
+     for i=2:size(parse($expr_str).args,1)]
+  end
+
+  return esc(ret_expr)
+
+end
